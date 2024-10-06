de082ca7dd4753e162c00d3fe95a7d905e5b96f9
Barbosa <76421572+barba-rossa@users.noreply.github.com>
2024-10-05 19:27:23 -0600

    adding check winner function

--- TicTacToe/TicTacToe/Form1.cs
+++ TicTacToe/TicTacToe/Form1.cs
@@ -27,8 +27,41 @@ namespace TicTacToe
             }
         }
 
-        // Continue
+        private void CheckForWinner()
+        {
+            if ((btn1.Text == btn2.Text && btn2.Text == btn3.Text && btn1.Text != "") ||
+                (btn4.Text == btn5.Text && btn5.Text == btn6.Text && btn4.Text != "") ||
+                (btn7.Text == btn8.Text && btn8.Text == btn9.Text && btn7.Text != "") ||
+                (btn1.Text == btn4.Text && btn4.Text == btn7.Text && btn1.Text != "") ||
+                (btn2.Text == btn5.Text && btn5.Text == btn8.Text && btn2.Text != "") ||
+                (btn3.Text == btn6.Text && btn6.Text == btn9.Text && btn3.Text != "") ||
+                (btn1.Text == btn5.Text && btn5.Text == btn9.Text && btn1.Text != "") ||
+                (btn3.Text == btn5.Text && btn5.Text == btn7.Text && btn3.Text != ""))
+            {
+                string winner = isXTurn ? "O" : "X";
+                MessageBox.Show(winner + " Wins!", "Game Over");
+                ResetGame();
+            }
+            else if (moveCount == 9)
+            {
+                MessageBox.Show("It's a draw!", "Game Over");
+                ResetGame();
+            }
+        }
 
+        private void ResetGame()
+        {
+            foreach (Control c in Controls)
+            {
+                if (c is Button && c.Tag == "play")
+                {
+                    ((Button)c).Enabled = true;
+                    ((Button)c).Text = "";
+                }
+            }
 
+            isXTurn = true;
+            moveCount = 0;
+        }
     }
 }
